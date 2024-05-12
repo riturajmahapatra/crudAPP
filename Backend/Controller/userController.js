58b3375d4304102730621f1de77fec7b24109161
Rituraj Mahapatra <riturajmahapatra@gmail.com>
2024-05-12 22:27:36 +0530

    delete user-controller added

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -51,6 +51,13 @@ export const update = async (req, res) => {
 //delete
 export const deleteUser = async (req, res) => {
   try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ msg: 'User Data Deleted' });
   } catch (error) {
     res.status(500).json({ error: error });
   }
